b5047e50b0a734a73247f5b0227ace91ca6481d5
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 <sabaudianin@gmail.com>
2024-11-06 18:57:07 +0100

    Make file structure, install Dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,8 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
 
 function App() {
-	const [count, setCount] = useState(0);
-
 	return (
 		<>
 			<div className="h-screen flex items-center justify-center bg-blue-100">
